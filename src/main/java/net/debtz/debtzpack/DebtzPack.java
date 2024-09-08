6437571b41381e0e417208f46f6e63fc8051664c
Braeden Audlin <125329807+25Braeden@users.noreply.github.com>
2024-09-08 17:44:29 -0400

    adding alexandrite

--- src/main/java/net/debtz/debtzpack/DebtzPack.java
+++ src/main/java/net/debtz/debtzpack/DebtzPack.java
@@ -1,6 +1,8 @@
 package net.debtz.debtzpack;
 
 import com.mojang.logging.LogUtils;
+import net.debtz.debtzpack.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -32,6 +34,8 @@ public class DebtzPack {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -45,6 +49,10 @@ public class DebtzPack {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        // Adding item to ingredient tab
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+        }
 
     }
 
